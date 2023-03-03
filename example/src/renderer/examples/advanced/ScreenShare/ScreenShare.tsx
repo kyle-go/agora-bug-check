41e72bcdadfc888cde330c7dcd20166575c7bc54
Kyle <kyle.script@gmail.com>
2023-03-03 23:28:17 +0800

    禁止 视频，音频，预览

--- example/src/renderer/examples/advanced/ScreenShare/ScreenShare.tsx
+++ example/src/renderer/examples/advanced/ScreenShare/ScreenShare.tsx
@@ -119,10 +119,11 @@ export default class ScreenShare
 
         // Need to enable video on this case
         // If you only call `enableAudio`, only relay the audio stream to the target channel
-        this.engine.enableVideo();
+        this.engine.disableVideo();
+        this.engine.disableAudio();
 
         // Start preview before joinChannel
-        this.engine.startPreview();
+        // this.engine.startPreview();
         this.setState({startPreview: true});
 
         this.getScreenCaptureSources();
